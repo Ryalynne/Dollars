b771fc27451c4db217b356bab10d6e157f4ac14a
Ryalynne <santiagoryan788@gmail.com>
2024-06-14 00:05:40 +0800

    new env

--- server/src/lib/DBPool.js
+++ server/src/lib/DBPool.js
@@ -1,10 +1,21 @@
+// const DBPool = {
+//   dbConfig: {
+//     host: `milkize-db.cfcckusoq29y.ap-southeast-2.rds.amazonaws.com`,
+//     user:`root`,
+//     password: `12345678`,
+//     database: `dollars_db`
+//   }
+// }
+
+// export default DBPool 
+
+
 const DBPool = {
   dbConfig: {
-    host: `milkize-db.cfcckusoq29y.ap-southeast-2.rds.amazonaws.com`,
-    user:`root`,
-    password: `12345678`,
-    database: `dollars_db`
+    host: `${process.env.MYSQL_PORT}`,
+    user:`${process.env.MYSQL_USER}`,
+    password: `${process.env.MYSQL_PASSWORD}`,
+    database: `${process.env.MYSQL_DATABASE}`
   }
 }
-
 export default DBPool 
